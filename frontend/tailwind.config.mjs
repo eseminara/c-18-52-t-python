e2ff8e3b5861b53e9c406a83618038905ea52c89
Hernan Casasola <67485423+GuidoMaxier@users.noreply.github.com>
2024-05-27 20:01:07 -0300

    add Navar Footeer - index

--- frontend/tailwind.config.mjs
+++ frontend/tailwind.config.mjs
@@ -9,6 +9,7 @@
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+	darkMode: 'media',
 	content: [
 		'./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}',
 		'./node_modules/flowbite/**/*.js'
